59cdbe021d756409f86a26583c52f6259f377e1d
Sumukh Barve <sumukhbarve@gmail.com>
2016-05-22 21:37:55 +0530

    Started serving index.html at '/'.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import os;
 import bottle;
 
+### Constants ::::::::::::::::::::::::::::::::::::::::::::::
+
 IS_PA = True if "PA_REPO_DIR" in os.environ else False;
+REPO_DIR = os.environ.get("PA_REPO_DIR", os.path.abspath("."));
+
+
+### Globals ::::::::::::::::::::::::::::::::::::::::::::::::
 
 request = bottle.request;
 response = bottle.response;
+staticFile = bottle.static_file;
 
 app = bottle.Bottle();
 
+### Routes :::::::::::::::::::::::::::::::::::::::::::::::::
+
 @app.get("/")
 def get_slash():
+    return staticFile("index.html", REPO_DIR);
+
+@app.get("/api/ping")
+def get_api_ping():
     response.content_type = "application/json";
     response.set_header("Access-Control-Allow-Origin", "*");
-    return {"version": bottle.__version__, "is_pa": IS_PA};
+    return {"status": "success"};
 
-# For running on PA, export `application`:
+# Running on PA ::::::::::::::::::::::::::::::::::::::::::::
 application = app;
 
-# Run locally:
+# Running locally ::::::::::::::::::::::::::::::::::::::::::
 if not IS_PA and __name__ == "__main__":
-    app.run(debug=True, reloader=True);
+    app.run(port=8000, debug=True, reloader=True);
